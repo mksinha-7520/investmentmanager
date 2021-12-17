351ab5ab9ea75353ebd8b3024fb3894224e7bdd1
Manish Sinha <mk.ssit@gmail.com>
2021-12-17 12:56:27 +0530

    initial commit

--- src/main/java/com/service/investmentmanager/InvestmentManagerApplication.java
+++ src/main/java/com/service/investmentmanager/InvestmentManagerApplication.java
@@ -2,8 +2,9 @@ package com.service.investmentmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class InvestmentManagerApplication {
 
 	public static void main(String[] args) {
